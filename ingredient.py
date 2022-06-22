a925da3f82b895407843365611bc83be9666b5df
Nabeel Rehemtulla <nabeelre@gmail.com>
2022-06-22 16:35:07 -0700

    removed is_signal

--- ingredient.py
+++ ingredient.py
@@ -1,10 +1,9 @@
 import matplotlib.pyplot as plt
 
 class Ingredient():
-    def __init__(self, func, label, is_signal=False, **kwargs):
+    def __init__(self, func, label, **kwargs):
         self.func = func
         self.kwargs = kwargs
-        self.is_signal = is_signal
         self.label = label
 
     def plot(self, x):
