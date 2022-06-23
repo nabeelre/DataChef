43f9ea62dfffc75e333fbfbdfd2b4ee305722fdb
Peggy-Guo <yiqingguo2022@u.northwestern.edu>
2022-06-23 14:32:41 -0700

    added doctrings to mix_function.py

--- mix_functions.py
+++ mix_functions.py
@@ -1,21 +1,61 @@
 import numpy as np
 
 def add(y1, y2):
-    "Return y1 + y2. "
+    '''Function to do addtion to ingredients.
+
+    Args:
+        y1 (:obj:`array`): numpy vector. The current ingredient.
+        y2 (:obj:`array`): numpy vector. The new ingredient.
+
+    Returns:
+        array: y1 + y2.
+    '''
     return np.add(y1, y2)
 
 def multiply(y1, y2):
-    "Return y1 * y2. "
+    '''Function to do mulplication to ingredients.
+
+    Args:
+        y1 (:obj:`array`): numpy vector. The current ingredient.
+        y2 (:obj:`array`): numpy vector. The new ingredient.
+
+    Returns:
+        array: y1 * y2
+    '''
     return np.multiply(y1, y2)
 
 def subtract(y1, y2):
-    "Return y1 - y2. "
+    '''Function to do subtraction to ingredients.
+
+    Args:
+        y1 (:obj:`array`): numpy vector. The current ingredient.
+        y2 (:obj:`array`): numpy vector. The new ingredient.
+
+    Returns:
+        array: y1 - y2
+    '''
     return np.subtract(y1, y2)
 
 def divide(y1, y2):
-    "Return y1 / y2. "
+    '''Function to do division to ingredients.
+
+    Args:
+        y1 (:obj:`array`): numpy vector. The current ingredient.
+        y2 (:obj:`array`): numpy vector. The new ingredient.
+
+    Returns:
+        array: y1 / y2
+    '''
     return np.divide(y1, y2)
 
 def convolve(y1, y2):
-    "Return convolution of y1 and y2"
+    '''Function to do concolution to ingredients.
+
+    Args:
+        y1 (:obj:`array`): numpy vector. The current ingredient.
+        y2 (:obj:`array`): numpy vector. The new ingredient.
+
+    Returns:
+        array: convolution of y1 and y2.
+    '''
     return np.convolve(y1, y2, "same")
